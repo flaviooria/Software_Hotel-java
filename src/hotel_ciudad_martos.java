3211fc04c9d972dadf942c5e85514fbd5afd22df
Flavio Oria <flavio.oriap@gmail.com>
2020-11-08 19:19:42 +0100

    se añadio el numero de billtes y monedas restantes

--- src/hotel_ciudad_martos.java
+++ src/hotel_ciudad_martos.java
@@ -2,8 +2,6 @@
 import java.util.*;
 import java.util.concurrent.TimeUnit;
 
-import static java.lang.Integer.parseInt;
-
 public class hotel_ciudad_martos {
     public static void main(String[] args) throws InterruptedException {
         //Declarando variables
@@ -15,10 +13,11 @@ public class hotel_ciudad_martos {
         int numeroReservas = 0, reserva = 0;
         boolean salidaSistema = false;
         long codigoHabitacion = 0;
-        int pagoTotal,precioHabitacion, pagoCliente;
+        int cambio,precioHabitacion, pagoCliente;
         int dia, mes, anio, diferenciaFechas = 0;
         float iva,monto;
-        int subTotal, total, ingresoTotal = 0,pago;
+        int subTotal, total, ingresoTotal = 0, pago;
+        int de100 = 20, de50 = 20, de20 = 20, de10 = 20, de5 = 20, de2= 20, de1= 20;
 
         do {
             //Menu de Bienvenida
@@ -48,12 +47,22 @@ public class hotel_ciudad_martos {
                 System.out.println("Desea una habitacion doble o individual (1/2)");
                 reserva = Integer.parseInt(s.nextLine());
 
+                Thread.sleep(500);
+                System.out.print("Comprobando disponibilidad ");
+                for (int i = 0; i < 3; i++) {
+                    System.out.print(".");
+                    TimeUnit.SECONDS.sleep(1);
+                }
+                System.out.println();
+
                 if (reserva == 2) {
                     System.out.println("Reserva satisfactoria");
+                    System.out.println();
                     habitacionesDobles -= 1;
                     numeroReservas++;
                 } else {
                     System.out.println("Reserva satisfactoria");
+                    System.out.println();
                     habitacionesIndividuales -= 1;
                     numeroReservas++;
                 }
@@ -121,28 +130,28 @@ public class hotel_ciudad_martos {
                 nombreCliente = s.nextLine();
 
                 //GENERAR FACTURA
-                System.out.println("|================================|");
-                System.out.println("|\t\t\tFactura              |");
-                System.out.println("|================================|");
-                System.out.printf("|\tDescripción\t\t %12s|\n",nombreCliente);
-                System.out.println("|\tConfirmación\t\t\t" + codigoHabitacion);
-                System.out.println("|\tEntrada\t\t\t\t" + fechaReserva + "|");
-                System.out.println("|\tSalida\t\t\t   " + dia + "/" + mes + "/" + anio + "|");
-                System.out.printf("|\tPax:\t\t\t %12d|\n",reserva);
-                System.out.printf("|\tNoches:\t\t\t %12d|\n",diferenciaFechas);
-                System.out.println("|================================|");
+                System.out.println("|==================================|");
+                System.out.println("|\t\t\t  Factura              |");
+                System.out.println("|==================================|");
+                System.out.printf("|\tDescripción\t\t %12s  |\n",nombreCliente);
+                System.out.println("|\tConfirmación\t\t\t" + codigoHabitacion + "  |");
+                System.out.println("|\tEntrada\t\t\t\t" + fechaReserva + "  |");
+                System.out.println("|\tSalida\t\t\t   " + dia + "/" + mes + "/" + anio + "  |");
+                System.out.printf("|\tPax:\t\t\t %12d  |\n",reserva);
+                System.out.printf("|\tNoches:\t\t\t %12d  |\n",diferenciaFechas);
+                System.out.println("|==================================|");
                 // comprobando si el numero de reserva es doble o individual de ese modo el precio varia
                 if (reserva == 2) {
                     total = precioHabitacion;
                     monto = (float) ( precioHabitacion  * 0.79);
                     iva = (float) (precioHabitacion * 0.21);
                     subTotal = total;
-                    System.out.printf("|\tTarifa:\t\t\t %12d|\n", precioDoble);
-                    System.out.printf("|\tMonto:\t\t\t%13.2f|\n",monto);
-                    System.out.printf("|\tsubtotal:\t\t\t%9d|\n",subTotal);
-                    System.out.printf("|\tIVA:\t\t\t%13.2f|\n",iva);
-                    System.out.printf("|\ttotal:\t\t\t%13d|\n",total);
-                    System.out.println("|================================|");
+                    System.out.printf("|\tTarifa:\t\t\t %12d  |\n", precioDoble);
+                    System.out.printf("|\tMonto:\t\t\t%13.2f  |\n",monto);
+                    System.out.printf("|\tSubtotal:\t\t\t%9d  |\n",subTotal);
+                    System.out.printf("|\tIVA:\t\t\t%13.2f  |\n",iva);
+                    System.out.printf("|\tTotal:\t\t\t%13d  |\n",total);
+                    System.out.println("|==================================|");
                     habitacionesDobles++;
                     ingresoTotal += total;
                 } else {
@@ -159,31 +168,37 @@ public class hotel_ciudad_martos {
                     habitacionesIndividuales++;
                     ingresoTotal += total;
                 }
+                System.out.println();
 
                 //PEDIR DINERO Y COMPROBAR LA VUELTA
                 System.out.println("Ingresa la cantidad con la cual va a pagar: ");
-                pago = Integer.parseInt(s.nextLine());
+                pagoCliente = Integer.parseInt(s.nextLine());
+                pago = pagoCliente - precioHabitacion;
+                cambio = pago;
 
                 int billetes100,billetes50,billete20,billete10,monedas5,monedas2,monedas1;
-                int de100 = 10, de50 = 20, de20 = 20, de10 = 20, de5 = 20, de2= 20, de1= 20;
                 int resultado100, resultado50, resultado20, resultado10, resultado5, resultado2, resultado1;
 
                 billetes100 = pago / 100;
                 if (billetes100 <= de100) {
                     pago = pago - (billetes100 * 100);
                     resultado100 = billetes100;
+                    de100 -= billetes100;
                 } else {
                     pago = pago - (de100 * 100);
                     resultado100 = de100;
+                    de100 = 0;
                 }
 
                 billetes50 = pago / 50;
                 if (billetes50 <= de50) {
                     pago = pago - (billetes50 * 50);
                     resultado50 = billetes50;
+                    de50 -= billetes50;
                 } else {
                     pago = pago - (de50 * 50);
                     resultado50 = de50;
+                    de50 = 0;
                 }
 
 
@@ -191,9 +206,11 @@ public class hotel_ciudad_martos {
                 if (billete20 <= de20) {
                     pago = pago - (billete20 * 20);
                     resultado20 = billete20;
+                    de20 -= billete20;
                 } else {
                     pago = pago - (de20 * 20);
                     resultado20 = de20;
+                    de20 = 0;
                 }
 
 
@@ -201,38 +218,47 @@ public class hotel_ciudad_martos {
                 if (billete10 <= de10) {
                     pago = pago - (billete10 * 10);
                     resultado10 = billete10;
+                    de10 -= billete10;
                 } else {
                     pago = pago - (de10 * 10);
                     resultado10 = de10;
+                    de10 = 0;
                 }
 
                 monedas5 = pago / 5;
                 if (monedas5 <= de5){
                     pago = pago - (monedas5 * 5);
                     resultado5 = monedas5;
+                    de5 -= monedas5;
                 } else {
                     pago = pago - (de5 * 5);
                     resultado5 = de5;
+                    de5 = 0;
                 }
 
                 monedas2 = pago / 2;
                 if (monedas2 <= de2) {
                     pago = pago - (monedas2 * 2);
                     resultado2 = monedas2;
+                    de2 -= monedas2;
                 } else {
                     pago = pago - (de2 * 2);
                     resultado2 = de2;
+                    de2 = 0;
                 }
 
                 monedas1 = pago / 1;
                 if (monedas1 <= de1) {
                     pago = pago - (monedas1 * 1);
                     resultado1 = monedas1;
+                    de1 -= monedas1;
                 } else {
                     pago = pago - (de1 * 1);
                     resultado1 = de1;
+                    de1 = 0;
                 }
 
+                System.out.println("Tu cambio es de: " + cambio);
                 if (billetes100 > 0) System.out.println("Cambio en billetes de 100 es: " +  resultado100);
                 if (billetes50 > 0) System.out.println("Cambio en billetes de 50 es: " +  resultado50);
                 if (billete20 > 0) System.out.println("Cambio en billetes de 20 es: " +  resultado20);
@@ -294,7 +320,6 @@ public class hotel_ciudad_martos {
 
                         // Ingresos y reseravas totales.
                         if (opcionAdmin.equals("1")) {
-
                             System.out.println("El numero de Ingresos totales son: " + ingresoTotal);
                             System.out.println("Número de reservas totales son: " + numeroReservas);
                             System.out.println("Pulsa una tecla para continuar");
@@ -303,19 +328,21 @@ public class hotel_ciudad_martos {
 
                         //Consultar cambio
                         if (opcionAdmin.equals("2")) {
-                          /*  System.out.println("Cambio en billetes de 100 es: " +  billetes100);
-                            System.out.println("Cambio en billetes de 50 es: " +  billetes50);
-                            System.out.println("Cambio en billetes de 20 es: " +  billete20);
-                            System.out.println("Cambio en billetes de 10 es: " +  billete10);
-                            System.out.println("Cambio en billetes de 5 es: " +  monedas5);
-                            System.out.println("Cambio en billetes de 1 es: " +  monedas1);*/
+                            Thread.sleep(1000);
+                            System.out.println("Billetes y monedas restantes");
+                            System.out.println("Billetes de 100 es: " +  de100);
+                            System.out.println("Billetes de 50 es: " +  de50);
+                            System.out.println("Billetes de 20 es: " +  de20);
+                            System.out.println("Billetes de 10 es: " +  de10);
+                            System.out.println("Monedas de 5 es: " +  de5);
+                            System.out.println("Monedas de 2 es: " +  de2);
+                            System.out.println("Monedas de 1 es: " +  de1);
                             System.out.println("Pulsa una tecla para continuar");
                             s.nextLine();
                         }
 
                         //Apagado del sistema
                         if (opcionAdmin.equals("3")) {
-
                             System.out.print("Apagando ");
                             for (int i = 0; i < 3; i++) {
                                 System.out.print(".");
